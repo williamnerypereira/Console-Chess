e512ce00d575143f51202ddab9571fa95e274584
williamnerypereira <william.nery.pereira@gmail.com>
2021-06-01 22:21:06 -0300

    Comecando a implmentar o tabuleiro

---
+++ xadrez_console/Tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tab tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tab tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+            this.tab = tab;
+        }
+    }
+}
---
+++ xadrez_console/Tabuleiro/Tab.cs
@@ -0,0 +1,17 @@
+﻿namespace Tabuleiro
+{
+    class Tab
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        private Peca[,] pecas;
+
+        public Tab(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
